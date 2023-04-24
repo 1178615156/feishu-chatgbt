1fbe8cfc8b7b6ef4ee591773af3397ae0ae331b1
yjs <yjs@weizhi.com>
2023-04-24 20:59:08 +0800

    update

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ logger.level(environ.get("LOG_LEVEL","INFO").upper())
 logger.configure(handlers=[dict(
     sink=sys.stdout,
     colorize=True,
-    format='<green>{time:YYYY-MM-DD HH:mm:ss.SSS}</green> | <level>{level: <8}</level> | <cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - <level> {extra[app_user]} {message}</level>',
+    format='<green>{time:YYYY-MM-DD HH:mm:ss.SSS}</green> | <level>{level: <8}</level> | <cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - <level> {message}</level>',
 )])
 app = Flask('bot')
 
