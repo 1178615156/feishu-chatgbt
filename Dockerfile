8bc21765ce5f44ebfd361b38847ac8afacc28843
yujiesui <yujieshui@weizhi.com>
2023-04-26 17:36:57 +0800

    update

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,18 @@
+FROM alpine:3 as model
+ARG HTTP_PROXY
+ENV HTTP_PROXY=$HTTP_PROXY
+
+RUN sed -i 's/dl-cdn.alpinelinux.org/mirrors.aliyun.com/g' /etc/apk/repositories
+RUN apt install unzip
+RUN cd \app &&  \
+    wget https://github.com/JaidedAI/EasyOCR/releases/download/v1.3/zh_sim_g2.zip -o zh_sim_g2.zip && \
+    unzip zh_sim_g2.zip && \
+    rm -f zh_sim_g2.zip
+RUN cd \app &&  \
+    wget https://github.com/JaidedAI/EasyOCR/releases/download/pre-v1.1.6/craft_mlt_25k.zip -o craft_mlt_25k.zip && \
+    unzip craft_mlt_25k.zip && \
+    rm -f craft_mlt_25k.zip
+
 
 FROM python:3.11-slim-bullseye
 WORKDIR /app
@@ -6,13 +21,15 @@ ENV LC_ALL=en_US.UTF-8 \
     LANGUAGE=en_US.UTF-8 \
     TZ=Asia/Shanghai \
     HOST=0.0.0.0
+RUN pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
+RUN pip install --no-cache-dir easyocr
+
 COPY sources.list /etc/apt/sources.list
 COPY requirements.txt /app/requirements.txt
-RUN pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
 # 常用包缓存,避免重复下载
 RUN pip install --no-cache-dir -r requirements.txt
-
 COPY . /app
+COPY --from=model /app ~/.EasyOCR/model
 CMD ["python","main.py"]
 
 
--- test.py
+++ test.py
@@ -5,3 +5,22 @@ def test_find_urls():
     urls = find_urls("hello https://1.1.1.1/996 world: zz https://baidu.com/996")
     print(urls)
     assert urls == ['https://1.1.1.1/996', 'https://baidu.com/996']
+
+
+import requests
+
+print(requests.get('http://www.scio.gov.cn/37234/Document/1738206/1738206.htm',
+
+                   headers={
+                       'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+                       'Accept-Encoding': 'gzip, deflate',
+                       'Accept-Language': 'zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7',
+                       'Cache-Control': 'no-cache',
+                       'Connection': 'keep-alive',
+                       'Cookie':
+                       'jsluid_h=b29dc15f44783cb6e3bc5a12d2787154; Hm_lvt_7cd4be5b075ad771f065c6fe4059883a=1681177476; Hm_lpvt_7cd4be5b075ad771f065c6fe4059883a=1682495300',
+                   'Pragma': 'no-cache',
+'Referer': 'http://www.scio.gov.cn/index.htm',
+'Upgrade-Insecure-Requests': '1',
+'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36'
+}).text)
