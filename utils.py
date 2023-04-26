afab24f8d3751239a1883f79f84c08e20218b09b
yujiesui <yujieshui@weizhi.com>
2023-04-26 14:47:00 +0800

    update

--- utils.py
+++ utils.py
@@ -8,13 +8,15 @@ def find_urls(text: str):
     :param url_index_start:
     :return:
     '''
+    http = 'http://'
+    http_len = len(http)
     https = "https://"
     https_len = len(https)
     i = 0
     results = []
     text_len = len(text)
     while i < text_len:
-        if text[i:i + https_len] == https:
+        if text[i:i + https_len] == https or text_len[i:i + http_len] == http:
             url_index_start = i
             while i < text_len and text[i] in (string.digits + string.ascii_letters + string.punctuation):
                 i += 1
